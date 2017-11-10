1b4e2e344b7302bffba7ec6be45d1b71ac6646d1
Gulzaib Tahir <gulzaib.tahir@devtrongenesis.com>
2017-11-10 13:10:21 +0500

    Initial Commit

---
+++ admin/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>XXL Sports and Wear Assessories</title>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1" name="viewport"><!-- Font Awsome -->
+  <link href="../font-awesome/css/font-awesome.css" rel="stylesheet">
+  <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet"><!-- Icon Style  -->
+  <link href="../css/IconSet/icon-set-new.css" rel="stylesheet"><!-- <link rel="stylesheet"  href="css/IconSet/fonts/untitled-font-3.ttf"> -->
+  <!-- Boot Strap -->
+  <link href="../bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link rel="icon" type="image/png" href="../img/logo.png">
+  <script src="../bootstrap/js/tests/vendor/jquery.min.js"></script>
+  <script src="../js/jquery-3.2.1.min.js"></script>
+  <script src="../bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- Style sheet -->
+  <script src="../js/script.js"> </script>
+  <link href="../css/style.css" rel="stylesheet" type="text/css">
+  <link href="../css/rwd.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="start">
+    <div class="container-fluid header">
+      <div class="top-nav col-lg-12 col-md-12">
+        <ul class="top-ul">
+          <li><a href="#"><i class="fa fa-star-o fa-lg" aria-hidden="true"></i>XXL BLOG</a></li>
+          <li><a href="#"><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i>OUR DEPARTMENT STORE</a></li>
+          <li><a href="#"><span class="glyphicons glyphicons-shopping-cart"></span>CUSTOMER SERVICE</a></li>
+          <li><a href="#"><i class="fa fa-user-o fa-lg" aria-hidden="true"></i>SIGN IN</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul class="top-items col-md-12 col-lg-12">
+          <li class="col-lg-2 col-md-2"><img src="../img/logo1.png"></li>
+          <li class="col-lg-2 col-md-2"><h4>ALL SPORTS UNITED</h4></li>
+          <li class="col-lg-5 col-md-5"><input type="search" name="search" class="form-control" placeholder="Search for Products"></li>
+          <li class="col-lg-3 col-md-3"><a href="#"><i class="fa fa-shopping-cart fa-3x" aria-hidden="true"></i><a>
+          <a href=""><h4>EMPTY SHOPPING CART</h4></a></li>
+        </ul>
+      </div>
+      <div class="second-nav">
+        <ul>
+          <li><a href="">WINTER SPORTS</a></li>
+          <li><a href="">CLOTHE</a></li>
+          <li><a href="">SHOES</a></li>
+          <li><a href="">HUNTING &amp; REACTION</a></li>
+          <li><a href="">SPORTS &amp; BALL GAMES</a></li>
+          <li><a href="">EXERCISE</a></li>
+          <li><a href="">ELECTRONICS</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+  session_start();
+  include('admin/connection.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>XXL Sports and Wear Assessories</title>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1" name="viewport"><!-- Font Awsome -->
+  <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"><!-- Icon Style  -->
+  <link href="css/IconSet/icon-set-new.css" rel="stylesheet"><!-- <link rel="stylesheet"  href="css/IconSet/fonts/untitled-font-3.ttf"> -->
+  <!-- Boot Strap -->
+  <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link rel="icon" type="image/png" href="img/logo.png">
+  <script src="bootstrap/js/tests/vendor/jquery.min.js">
+  </script>
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="bootstrap/dist/js/bootstrap.min.js">
+  </script>
+  <!-- Style sheet -->
+  <script src="js/script.js"> </script>
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <link href="css/rwd.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <?php 
+    include('admin/header.php');
+  ?>
+  <div class="">
+    <div class="header">
+      <div class="home-back">
+        <h1>Extreme party purchases!</h1>
+        <p><a href="#">Read this week's customer magazine here!</a></p>
+        <div>
+          <img src="../img/1.jpg" class="col-lg-6">
+          <img src="../img/2.jpg" class="col-lg-3">
+          <img src="../img/3.jpg" class="col-lg-3">
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php 
+    include('admin/footer.php');
+  ?>
+</body>
+</html>
+
+
+
+
